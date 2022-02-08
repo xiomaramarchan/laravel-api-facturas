d910b7fb185b0c5b5ba2db88e2de37b1753f47de
Xiomara Marchan <xiomaramarchan1981@gmail.com>
2022-02-08 15:03:08 -0400

    Actualizacion Modelo User

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,35 +10,41 @@ use Laravel\Passport\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasFactory, Notifiable, HasApiTokens;
 
     /**
      * The attributes that are mass assignable.
      *
-     * @var array<int, string>
+     * @var array
      */
     protected $fillable = [
         'name',
         'email',
+        'nit',
         'password',
     ];
 
     /**
-     * The attributes that should be hidden for serialization.
+     * The attributes that should be hidden for arrays.
      *
-     * @var array<int, string>
+     * @var array
      */
     protected $hidden = [
+    
         'password',
         'remember_token',
     ];
 
     /**
-     * The attributes that should be cast.
+     * The attributes that should be cast to native types.
      *
-     * @var array<string, string>
+     * @var array
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function facturas() {
+        return $this->hasMany(Factura::class);
+    }
 }
